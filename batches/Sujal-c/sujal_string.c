8b943f494563650500fe4b767f18703b22b447e6
Harsh_Sonara <harshsonara77660@gmail.com>
2025-02-04 17:44:15 +0530

    String & Type conversion

---
+++ batches/Sujal-c/sujal_string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	char x = 'h'; 
+  	char myText[] = "Hello";
+ 
+ 	printf("character is :- %c ",x);
+  	printf("\n%s", myText);
+  	
+  return 0;
+}
---
+++ batches/Sujal-c/type_conv.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	
+	float x = (float) 5/2;
+	
+	printf("x = %.1f",x);
+  	
+  return 0;
+}
