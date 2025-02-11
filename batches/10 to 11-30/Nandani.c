683430cc010c45e7f472640eae15046646dc0175
Harsh_Sonara <harshsonara77660@gmail.com>
2025-02-11 11:32:24 +0530

    new batch codes

---
+++ batches/10 to 11-30/Nandani.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(){
+//		
+//		//Introduction
+//	  printf("Hello World!");
+//	  
+//	  //name 
+//	  printf("\nMy name is Mihir Sharma");
+//	  
+//	  /*i am using next line 
+//	  this method \n */
+//	  
+//	  printf("/n/n I am using backslash \n for next line.");
+	  
+	  int Number = 21;
+	  Number = 25;
+	  int OtherNumber;
+	  
+	  OtherNumber = Number;
+	  
+	  float Marks = 73.73;
+	  char Grade = 'A';
+	  
+	  printf("My roll no. is = %d\n",OtherNumber + Number);
+	  
+	  printf("My Percentage is = %.2f\n",Marks);
+	  
+	  printf("My Grade is = %c\n",Grade);
+	  
+	  int x, y, z;
+	  
+	  printf("X = ",x);
+	  scanf("%d",&x);
+	  
+	  printf("\nY = ",y);
+	  scanf("%d",&y);
+	  
+	  printf("\nZ = ",z);
+	  scanf("%d",&z);
+	  
+	  printf("\nsum of xyz = %d",x+y+z);
+	  	  
+	return 0;
+}	
