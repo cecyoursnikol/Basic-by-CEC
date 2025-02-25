68dde27ca55543ce5d997c1d83bb06fd43c9c20d
Harsh_Sonara <harshsonara77660@gmail.com>
2025-02-25 16:25:04 +0530

    updates

--- batches/6 to 7-30/Char_.c
+++ batches/6 to 7-30/Char_.c
@@ -5,8 +5,8 @@ int main(){
 	char x = 'H';
 	char sujal[] = "Hello";
 	
-	printf("Character is :- %c",x);
-		printf("Character is :- %s",sujal[]);
+	printf("Character is :- %c\n",x);
+	printf("Character is :- %s",sujal);
 
 	
 	return 0;
