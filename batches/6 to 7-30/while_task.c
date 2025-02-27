398adcefdb1d6da8244e09831572275bce6327f3
Harsh_Sonara <harshsonara77660@gmail.com>
2025-02-27 17:46:38 +0530

    update

--- batches/6 to 7-30/while_task.c
+++ batches/6 to 7-30/while_task.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int limit, i = 1; 
+    int limit, i; 
 
+	printf("Enter a number: ",i);
+    scanf("%d", &i);
     // Taking user input for loop limit
     printf("Enter a number to count up to: ");
     scanf("%d", &limit);
