5fa2911400e3ee27995a7cc57cd761e1eed3ea17
Harsh_Sonara <harshsonara77660@gmail.com>
2025-03-03 16:36:23 +0530

    new batch'

---
+++ batches/3.30 TO 5.00/second.c
@@ -0,0 +1,15 @@
+#include <stdio.h> //header file (standard input output)
+
+int main() {
+	
+	int age = 19;
+	float percentage = 74.85;
+	char grade = 'A';
+	
+	printf("Age of Prince :- %d",age);
+	printf("\nResult of Prince :- %.2f",percentage);
+	printf("\nGrade of Prince :- %c",grade);
+	
+	
+	return 0;
+}
