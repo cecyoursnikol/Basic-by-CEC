f47f252956b0b51cd913bf553a91199c846b83de
Harsh_Sonara <harshsonara77660@gmail.com>
2025-03-04 14:31:16 +0530

    update

---
+++ batches/10 to 11-30/else_if.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+	
+	int a,b;
+	
+	printf("Enter number for A:- ",a);
+	scanf("%d",&a);
+	
+	printf("\nEnter number for B:- ",b);
+	scanf("%d",&b);
+	
+	if(a == b){
+		printf("A is equal to B.");
+	}
+	else if (a > b){
+		printf("A is greater than b");
+	}
+	else if(a < b){
+		printf("A is less than b");
+	}
+	else {
+		printf("ERROR!");
+	}
+	
+	
+	return 0;
+}
