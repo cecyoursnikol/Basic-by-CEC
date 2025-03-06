4858fa296f7544da88a7d03218a6b929ffb2273e
Harsh_Sonara <harshsonara77660@gmail.com>
2025-03-06 18:50:33 +0530

    updates

--- batches/6 to 7-30/const.c
+++ batches/6 to 7-30/const.c
@@ -2,13 +2,13 @@
  int main (){
  	
  	const char Name = 'H';
-// 	 Name = 'F';
+ 	 Name = 'F';
  	printf("The Name Of The Person is %c",Name);
- 	
- 	const int Age = 22;
- 	const Age = 25;
- 	printf("\nThe Age Of The Person is %d",Age);
- 	
+// 	
+// 	const int Age = 22;
+//// 	const Age = 25;
+// 	printf("\nThe Age Of The Person is %d",Age);
+// 	
  	
  	return 0;
  }
