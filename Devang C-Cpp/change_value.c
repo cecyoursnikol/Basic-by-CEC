095a118084f1478098126f2b39115879507c341f
Harsh_Sonara <harshsonara77660@gmail.com>
2025-04-17 11:38:56 +0530

    Updates

---
+++ Devang C-Cpp/change_value.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	
+	int x = 10;
+	x = 15; // change value
+	int y = 20;
+	
+	int a = 5, b = 10, c = 15, d = 20; // storing multiple variables
+		
+ 	printf("%d\n",x+y);
+	
+	printf("\nA = %d\nB = %d\nC = %d\nD = %d",a,b,c,d);
+	
+	printf("\n\nSum of A,B,C & D = %d",a+b+c+d);
+	
+	return 0;
+}
