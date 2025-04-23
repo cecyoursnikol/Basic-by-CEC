1677b14f3f2f4d37489e39d46aba9c2ed4675365
Harsh_Sonara <harshsonara77660@gmail.com>
2025-04-23 18:51:52 +0530

    updated

---
+++ batches/6 to 7-30/scanf.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	
+	int x,y,z;
+	
+	printf("Enter your Value for X:- ",x);
+	scanf("%d",&x);
+	
+	printf("Enter your Value for Y:- ",y);
+	scanf("%d",&y);
+	
+	printf("Enter your Value for Z:- ",z);
+	scanf("%d",&z);
+	
+	printf("\nX = %d\nY = %d\nZ = %d\n",x,y,z);
+	
+	printf("\nAddition of X/Y/Z = %d",x+y+z);
+	
+	
+	return 0;
+}
